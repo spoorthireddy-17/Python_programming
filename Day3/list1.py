fd59f397d0af7d946c84afd90e6f2f21b82da60b
spoorthireddy-17 <spoorthireddy006@gmail.com>
2025-09-10 18:52:20 +0530

    Delete Day3 directory

--- Day3/list1.py
+++
@@ -1,9 +0,0 @@
-def addEle(n):
-    l=[]
-    for i in range(n):
-        e=input()
-        l.append(e)
-    return l
-n=int(input("enter no of elements:"))
-res=addEle(n)
-print(res)
--- Day3/set.py
+++
@@ -1,6 +0,0 @@
-myset=set()
-n=int(input('enter number of elements:'))
-for i in range(n):
-    inp=int(input())
-    myset.add(inp)
-print(myset)
